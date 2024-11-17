536952d48884cb7dfa301d8c48acd22ffa620e43
yinkeet <wongyinkeet@gmail.com>
2024-11-17 16:34:44 +0800

    Added direction collision ring buffer and made the old direction ring buffer contain direction, less confusing

--- engine/lib/direction.ts
+++ engine/lib/direction.ts
@@ -11,6 +11,17 @@ export enum Direction {
     NORTHWEST
 }
 
-export const directionRingBuffer = new RingBuffer(8, 9, 1, 5, 4, 6, 2, 10);
+export const directionRingBuffer = new RingBuffer(
+    Direction.NORTH, 
+    Direction.NORTHEAST, 
+    Direction.EAST, 
+    Direction.SOUTHEAST, 
+    Direction.SOUTH, 
+    Direction.SOUTHWEST, 
+    Direction.WEST, 
+    Direction.NORTHWEST
+);
+
+export const directionCollisionRingBuffer = new RingBuffer(8, 9, 1, 5, 4, 6, 2, 10);
 
 export const translationRingBuffer =  new RingBuffer<[number, number]>([0,1], [1,1], [1,0], [1,-1], [0,-1], [-1,-1], [-1,0], [-1,1])
