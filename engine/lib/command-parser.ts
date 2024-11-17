392fb10ecced0abeddf0846c73e21a3b12795b56
yinkeet <wongyinkeet@gmail.com>
2024-11-17 21:24:37 +0800

    Command parser is made to echo everything out

--- engine/lib/command-parser.ts
+++ engine/lib/command-parser.ts
@@ -1,7 +1,9 @@
 export const parseCommand = (command: string): [string, string[]] => {
-    const [argc, argv] = command.toUpperCase().split(' ')
+    command = command.toUpperCase();
+    console.log(command);
+    const [argc, argv] = command.split(' ');
     if (argv !== undefined) {
-        return [argc, argv.split(',')]
+        return [argc, argv.split(',')];
     }
-    return [argc, argv]
+    return [argc, argv];
 };
