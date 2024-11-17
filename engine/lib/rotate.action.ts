525e4be7d5a5a4417bb14c97850bd189c3c4434b
yinkeet <wongyinkeet@gmail.com>
2024-11-17 16:43:42 +0800

    Added rotate action class and tests

---
+++ engine/lib/rotate.action.ts
@@ -0,0 +1,23 @@
+import { Action } from './action.ts';
+import { directionRingBuffer, translationRingBuffer } from '/lib/direction.ts';
+import { Metadata } from './metadata.ts';
+
+export class RotateAction implements Action {
+    constructor(public metadata: Metadata, public angularStep: number) {}
+
+    perform(_args?: string[]): boolean {
+        // Get current index on ring buffer
+        const currentIndex = this.metadata.direction;
+
+        // New direction
+        this.metadata.direction = directionRingBuffer.get(
+            currentIndex + this.angularStep,
+        );
+
+        // Get new translation offset by moving the index by -2 on the ring buffer
+        this.metadata.translationOffset = translationRingBuffer.get(
+            currentIndex + this.angularStep,
+        );
+        return true;
+    }
+}
---
+++ engine/tests/lib/rotate.action_test.ts
@@ -0,0 +1,73 @@
+import { levels } from '/levels/index.ts';
+import { Metadata } from '../../lib/metadata.ts';
+import { Direction, translationRingBuffer } from '/lib/direction.ts';
+import { RotateAction } from '/lib/rotate.action.ts';
+import { assertEquals } from 'jsr:@std/assert';
+
+Deno.test('Rotate action - right', () => {
+    const metadata: Metadata = {
+        x: 0,
+        y: 0,
+        direction: Direction.NORTH,
+        translationOffset: translationRingBuffer.get(Direction.NORTH),
+        levelData: levels['5-5-blank'],
+    };
+    const action = new RotateAction(metadata, 2);
+    assertEquals(action.perform(undefined), true);
+    assertEquals(metadata.x, 0);
+    assertEquals(metadata.y, 0);
+    assertEquals(metadata.direction, Direction.EAST);
+    assertEquals(metadata.translationOffset, [1, 0]);
+
+    assertEquals(action.perform(undefined), true);
+    assertEquals(metadata.x, 0);
+    assertEquals(metadata.y, 0);
+    assertEquals(metadata.direction, Direction.SOUTH);
+    assertEquals(metadata.translationOffset, [0, -1]);
+
+    assertEquals(action.perform(undefined), true);
+    assertEquals(metadata.x, 0);
+    assertEquals(metadata.y, 0);
+    assertEquals(metadata.direction, Direction.WEST);
+    assertEquals(metadata.translationOffset, [-1, 0]);
+
+    assertEquals(action.perform(undefined), true);
+    assertEquals(metadata.x, 0);
+    assertEquals(metadata.y, 0);
+    assertEquals(metadata.direction, Direction.NORTH);
+    assertEquals(metadata.translationOffset, [0, 1]);
+});
+
+Deno.test('Rotate action - left', () => {
+    const metadata: Metadata = {
+        x: 0,
+        y: 0,
+        direction: Direction.NORTH,
+        translationOffset: translationRingBuffer.get(Direction.NORTH),
+        levelData: levels['5-5-blank'],
+    };
+    const action = new RotateAction(metadata, -2);
+    assertEquals(action.perform(undefined), true);
+    assertEquals(metadata.x, 0);
+    assertEquals(metadata.y, 0);
+    assertEquals(metadata.direction, Direction.WEST);
+    assertEquals(metadata.translationOffset, [-1, 0]);
+
+    assertEquals(action.perform(undefined), true);
+    assertEquals(metadata.x, 0);
+    assertEquals(metadata.y, 0);
+    assertEquals(metadata.direction, Direction.SOUTH);
+    assertEquals(metadata.translationOffset, [0, -1]);
+
+    assertEquals(action.perform(undefined), true);
+    assertEquals(metadata.x, 0);
+    assertEquals(metadata.y, 0);
+    assertEquals(metadata.direction, Direction.EAST);
+    assertEquals(metadata.translationOffset, [1, 0]);
+
+    assertEquals(action.perform(undefined), true);
+    assertEquals(metadata.x, 0);
+    assertEquals(metadata.y, 0);
+    assertEquals(metadata.direction, Direction.NORTH);
+    assertEquals(metadata.translationOffset, [0, 1]);
+});
