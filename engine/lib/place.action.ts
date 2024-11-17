96ba98708b7f816bf22f9bffc82aecabd3921fab
yinkeet <wongyinkeet@gmail.com>
2024-11-17 11:22:30 +0800

    Place action class with tests

---
+++ engine/lib/place.action.ts
@@ -0,0 +1,33 @@
+import { Action } from './action.ts';
+import { Metadata } from './metadata.ts';
+import { Direction, translationRingBuffer } from './direction.ts';
+import { z } from 'npm:zod';
+
+export class PlaceAction implements Action {
+    constructor(public metadata: Metadata) {}
+
+    perform(args: string[] | undefined): boolean {
+        if (args === undefined) return false;
+        if (args.length != 3) return false;
+
+        args[2] = args[2].toUpperCase();
+
+        const numberSchema = z.coerce.number().nonnegative()
+        const directionSchema = z.enum(['NORTH', 'SOUTH', 'EAST', 'WEST'])
+
+        try {
+            numberSchema.parse(args[0]);
+            numberSchema.parse(args[1]);
+            directionSchema.parse(args[2]);
+        } catch(_error) {
+            return false;
+        }
+
+        this.metadata.x = parseInt(args[0])
+        this.metadata.y = parseInt(args[1])
+        const direction = Direction[args[2] as keyof typeof Direction];
+        this.metadata.direction = direction;
+        this.metadata.translationOffset = translationRingBuffer.get(direction)[0];
+        return true;
+    }
+}
---
+++ engine/tests/lib/place.action_test.ts
@@ -0,0 +1,69 @@
+import { Metadata } from '../../lib/metadata.ts';
+import { Direction, translationRingBuffer } from '/lib/direction.ts';
+import { PlaceAction } from '/lib/place.action.ts';
+import { assertEquals } from 'jsr:@std/assert';
+
+Deno.test('Place action with undefined arguments', () => {
+    const action = new PlaceAction({
+        x: -1,
+        y: -1,
+        direction: Direction.NORTH,
+        translationOffset: translationRingBuffer.get(Direction.NORTH)[0]
+    });
+    assertEquals(action.perform(undefined), false);
+});
+
+Deno.test('Place action with insufficient arguments', () => {
+    const action = new PlaceAction({
+        x: -1,
+        y: -1,
+        direction: Direction.NORTH,
+        translationOffset: translationRingBuffer.get(Direction.NORTH)[0]
+    });
+    assertEquals(action.perform(['a', '1']), false);
+});
+
+Deno.test('Place action with invalid x argument', () => {
+    const action = new PlaceAction({
+        x: -1,
+        y: -1,
+        direction: Direction.NORTH,
+        translationOffset: translationRingBuffer.get(Direction.NORTH)[0]
+    });
+    assertEquals(action.perform(['a', '1', 'NORTH']), false);
+});
+
+Deno.test('Place action with invalid y argument', () => {
+    const action = new PlaceAction({
+        x: -1,
+        y: -1,
+        direction: Direction.NORTH,
+        translationOffset: translationRingBuffer.get(Direction.NORTH)[0]
+    });
+    assertEquals(action.perform(['1', 'a', 'NORTH']), false);
+});
+
+Deno.test('Place action with invalid direction argument', () => {
+    const action = new PlaceAction({
+        x: -1,
+        y: -1,
+        direction: Direction.NORTH,
+        translationOffset: translationRingBuffer.get(Direction.NORTH)[0]
+    });
+    assertEquals(action.perform(['1', 'a', 'SOMETHING']), false);
+});
+
+Deno.test('Place action with valid arguments', () => {
+    const metadata: Metadata = {
+        x: -1,
+        y: -1,
+        direction: Direction.NORTH,
+        translationOffset: translationRingBuffer.get(Direction.NORTH)[0]
+    };
+    const action = new PlaceAction(metadata);
+    assertEquals(action.perform(['1', '2', 'SOUTH']), true);
+    assertEquals(metadata.x, 1);
+    assertEquals(metadata.y, 2);
+    assertEquals(metadata.direction, Direction.SOUTH);
+    assertEquals(metadata.translationOffset, [0,-1]);
+});
