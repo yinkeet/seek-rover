7646e05d19e7d452be634a27e66f2e807a1dae1d
yinkeet <wongyinkeet@gmail.com>
2024-11-17 11:53:22 +0800

    Added types to level maps

--- engine/levels/index.ts
+++ engine/levels/index.ts
@@ -1,5 +1,17 @@
 import blank_5_5 from "./blank-5-5.json" with { type: "json" };
 
-export const levels = {
-    "5-5-blank": blank_5_5
+export interface Level {
+    [id : string]: LevelMap
+}
+
+export interface LevelMap {
+    [id: string]: Tile
+}
+
+export interface Tile {
+    wall: number
+}
+
+export const levels: Level = {
+    "5-5-blank": blank_5_5 as LevelMap
 }
