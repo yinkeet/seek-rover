827709d50f77c5d936d51f492502f45e85efaea8
yinkeet <wongyinkeet@gmail.com>
2024-11-18 23:37:50 +0800

    Added new state for level select

--- engine/lib/gamestate.ts
+++ engine/lib/gamestate.ts
@@ -1,5 +1,6 @@
 export enum GameState {
     Loading,   // Load level, configs, metadata and logics
+    LevelSelect,    // Select level
     Init,   // Waiting for rover placement
     Running // All commands accepted here
 }
